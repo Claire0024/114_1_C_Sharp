5d2c59561a48a0adf1abdc6fca52b883c5a53541
Claire0024 <a114221015@mail.shu.edu.tw>
2025-09-24 13:01:20 +0800

    1140924

---
+++ 114_09_24/114_1_C_Sharp_Tutorial-main/114_09_17/Tutorial_2_3/Tutorial_2_3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Tutorial_2_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.italian = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(106, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "選擇一個語言，我會說早安";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(147, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(513, 82);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // italian
+            // 
+            this.italian.Location = new System.Drawing.Point(100, 329);
+            this.italian.Name = "italian";
+            this.italian.Size = new System.Drawing.Size(142, 77);
+            this.italian.TabIndex = 2;
+            this.italian.Text = "義大利文";
+            this.italian.UseVisualStyleBackColor = true;
+            this.italian.Click += new System.EventHandler(this.italian_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "德文";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(585, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 77);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "西班牙文";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.italian);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "語言翻譯器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button italian;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 114_09_24/tutorial__2_5/tutorial__2_5/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace tutorial__2_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardFace_pictureBox = new System.Windows.Forms.PictureBox();
+            this.cardBack_pictureBox = new System.Windows.Forms.PictureBox();
+            this.showBackButton = new System.Windows.Forms.Button();
+            this.showFaceButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFace_pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBack_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardFace_pictureBox
+            // 
+            this.cardFace_pictureBox.Image = global::tutorial__2_5.Properties.Resources.Ace_Spades;
+            this.cardFace_pictureBox.Location = new System.Drawing.Point(409, 73);
+            this.cardFace_pictureBox.Name = "cardFace_pictureBox";
+            this.cardFace_pictureBox.Size = new System.Drawing.Size(160, 237);
+            this.cardFace_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardFace_pictureBox.TabIndex = 1;
+            this.cardFace_pictureBox.TabStop = false;
+            // 
+            // cardBack_pictureBox
+            // 
+            this.cardBack_pictureBox.Image = global::tutorial__2_5.Properties.Resources.Backface_Blue;
+            this.cardBack_pictureBox.Location = new System.Drawing.Point(135, 73);
+            this.cardBack_pictureBox.Name = "cardBack_pictureBox";
+            this.cardBack_pictureBox.Size = new System.Drawing.Size(160, 237);
+            this.cardBack_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardBack_pictureBox.TabIndex = 0;
+            this.cardBack_pictureBox.TabStop = false;
+            this.cardBack_pictureBox.Visible = false;
+            // 
+            // showBackButton
+            // 
+            this.showBackButton.Location = new System.Drawing.Point(168, 349);
+            this.showBackButton.Name = "showBackButton";
+            this.showBackButton.Size = new System.Drawing.Size(105, 59);
+            this.showBackButton.TabIndex = 2;
+            this.showBackButton.Text = "顯示背面";
+            this.showBackButton.UseVisualStyleBackColor = true;
+            // 
+            // showFaceButton
+            // 
+            this.showFaceButton.Location = new System.Drawing.Point(436, 349);
+            this.showFaceButton.Name = "showFaceButton";
+            this.showFaceButton.Size = new System.Drawing.Size(105, 59);
+            this.showFaceButton.TabIndex = 3;
+            this.showFaceButton.Text = "顯示正面";
+            this.showFaceButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showFaceButton);
+            this.Controls.Add(this.showBackButton);
+            this.Controls.Add(this.cardFace_pictureBox);
+            this.Controls.Add(this.cardBack_pictureBox);
+            this.Name = "Form1";
+            this.Text = "翻轉樸克牌";
+            ((System.ComponentModel.ISupportInitialize)(this.cardFace_pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBack_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardBack_pictureBox;
+        private System.Windows.Forms.PictureBox cardFace_pictureBox;
+        private System.Windows.Forms.Button showBackButton;
+        private System.Windows.Forms.Button showFaceButton;
+    }
+}
+
